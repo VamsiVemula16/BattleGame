08cc5ab967049291b3a14ccfcec719a0e77425eb
VamsiVemula16 <48033726+VamsiVemula16@users.noreply.github.com>
2020-01-23 13:30:23 -0600

    Initial Commit
    initial Commit

---
+++ Classes/inventory.py
@@ -0,0 +1,8 @@
+
+
+class Item:
+    def __init__(self, name, typed, description, prop):
+        self.name = name
+        self.type = typed
+        self.description = description
+        self.prop = prop
